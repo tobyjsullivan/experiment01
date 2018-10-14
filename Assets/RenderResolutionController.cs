44b42e1c9dff7fbd69428c273e8c82038251c29a
Toby Sullivan <tobyjsullivan@gmail.com>
2018-10-14 16:51:27 +1100

    Initial commit

---
+++ Assets/RenderResolutionController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class RenderResolutionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		XRSettings.eyeTextureResolutionScale = 1.5f;
+	}
+
+}
